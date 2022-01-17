efc0be6493356e0d43ce53cece0cbae4e61caca9
Austin Godwin <austingodwin18@gmail.com>
2022-01-17 23:16:00 +0100

    Grocery Item

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -4,3 +4,58 @@ import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 import '../models/models.dart';
+
+class GroceryItemScreen extends StatefulWidget {
+  final Function(GroceryItem) onCreate;
+  final Function(GroceryItem) onUpdate;
+  final GroceryItem? originalItem;
+  final bool isUpdating;
+  const GroceryItemScreen({
+    Key? key,
+    required this.onCreate,
+    required this.onUpdate,
+    this.originalItem,
+  })  : isUpdating = (originalItem != null),
+        super(key: key);
+
+  @override
+  _GroceryItemScreenState createState() => _GroceryItemScreenState();
+}
+
+class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
+
+  @override
+  void initState() {
+    final originalItem = widget.originalItem;
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _name = originalItem.name;
+      _currentSliderValue = originalItem.quantity;
+      _importance = originalItem.importance;
+      _currentColor = originalItem.color;
+      final date = originalItem.date;
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      _dueDate = date;
+    }
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+    );
+  }
+}
