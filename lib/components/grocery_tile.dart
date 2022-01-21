0aea5914556d1f971f6caa855b4809f0b79b682f
Austin Godwin <austingodwin18@gmail.com>
2022-01-21 20:36:27 +0100

    Grocery tile Construct

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -3,3 +3,11 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
 import '../models/grocery_item.dart';
 
+class GroceryTile extends StatelessWidget {
+  const GroceryTile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
