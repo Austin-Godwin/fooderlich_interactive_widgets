329d22a16d76ffdce39e50b3e8d53a1cd3466cf0
Austin Godwin <austingodwin18@gmail.com>
2022-01-22 21:09:25 +0100

    Grocery Tile Shown

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -23,29 +23,49 @@ class GroceryTile extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Container(
-            width: 5.0,
-            color: item.color,
-          ),
-          const SizedBox(
-            width: 16.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Row(
             children: [
-              Text(
-                item.name,
-                style: GoogleFonts.lato(
-                    decoration: textDecoration,
-                    fontSize: 21.0,
-                    fontWeight: FontWeight.bold),
+              Container(
+                width: 5.0,
+                color: item.color,
               ),
+              const SizedBox(
+                width: 16.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    item.name,
+                    style: GoogleFonts.lato(
+                        decoration: textDecoration,
+                        fontSize: 21.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  buildDate(),
+                  const SizedBox(
+                    height: 4.0,
+                  ),
+                  buildImportance()
+                ],
+              ),
+
             ],
           ),
-          const SizedBox(height: 4.0,),
-          buildDate(),
-          const SizedBox(height: 4.0,),
-          buildImportance()
+          Row(
+            children: [
+              Text(
+                item.quantity.toString(),
+                style: GoogleFonts.lato(
+                    decoration: textDecoration, fontSize: 21.0),
+              ),
+              buildCheckbox(),
+            ],
+          )
         ],
       ),
     );
